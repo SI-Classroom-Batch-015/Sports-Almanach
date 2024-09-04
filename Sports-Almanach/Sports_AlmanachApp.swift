3138aead8ee4606679c21ad8ea6cbe9a1ec32d91
Michael Fleps <f.michi84.989@gmail.com>
2024-09-04 16:34:01 +0200

    Initial Commit

---
+++ Sports-Almanach/Sports_AlmanachApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sports_AlmanachApp.swift
+//  Sports-Almanach
+//
+//  Created by Michael Fleps on 04.09.24.
+//
+
+import SwiftUI
+
+@main
+struct Sports_AlmanachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
