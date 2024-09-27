858b309d2ac08e3e831563ba43af12cd62febfd5
Michael Fleps <f.michi84.989@gmail.com>
2024-09-27 15:25:35 +0200

    Enum Season erweitert , später API Call mittels ausgewählter Jahreszahl

--- Sports-Almanach/Enums/Season.swift
+++ Sports-Almanach/Enums/Season.swift
@@ -7,6 +7,8 @@
 
 /// Jahreszahlenbereiche (Saison)
 enum Season: String, Identifiable, CaseIterable {
+    case season201872018 = "2017-2018"
+    case season2018_2019 = "2018-2019"
     case season2019_2020 = "2019-2020"
 
     
