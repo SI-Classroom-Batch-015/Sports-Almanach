eff87056ffaa5eb2380429818fb3617b3a8ddcb3
Michael Fleps <f.michi84.989@gmail.com>
2024-09-27 11:46:07 +0200

    eventRow erstellt

---
+++ Sports-Almanach/Views/List/Rows/EventRow.swift
@@ -0,0 +1,75 @@
+//
+//  EventRow.swift
+//  Sports-Almanach
+//
+//  Created by Michael Fleps on 27.09.24.
+//
+
+import SwiftUI
+
+struct EventRow: View {
+    
+    let event: Event
+    
+    var body: some View {
+        
+        HStack {
+            // Thumbnail
+            AsyncImage(url: URL(string: event.thumbURL)) { image in
+                image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 60, height: 60)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+            } placeholder: {
+                ProgressView()
+            }
+            
+            // Event Name, Datum/Uhrzeit
+            VStack(alignment: .leading) {
+                Text(event.name)
+                    .font(.headline)
+                    .lineLimit(2)
+                
+                HStack(spacing: 5) {
+                    Text(event.date)
+                        .font(.caption) // Kleinere Schriftart
+                        .foregroundColor(.gray)
+                    
+                    Text(event.time)
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                }
+            }
+            .padding(.leading, 10)
+            
+            Spacer()
+            
+            // Status und Pfeil
+            HStack {
+                Text(event.status.currentStatusGerman)
+                    .font(.subheadline)
+                    .padding(6)
+                    .background(event.status.color)
+                    .cornerRadius(8)
+                    .foregroundColor(.white)
+                
+                Image(systemName: "chevron.right")
+                    .foregroundColor(.gray)
+            }
+            .frame(width: 100)
+        }
+        .padding()
+        .overlay(
+            RoundedRectangle(cornerRadius: 15)
+                .stroke(Color.gray, lineWidth: 2)
+        )
+        .padding(.vertical, 8)
+    }
+}
+
+#Preview {
+    let mockEvent = MockEvents.events.first!
+    return EventRow(event: mockEvent)
+        .padding()
+}
