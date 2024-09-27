75b4db47c172f6cde7b13ce6d53a0490c9193854
Michael Fleps <f.michi84.989@gmail.com>
2024-09-27 15:23:27 +0200

    Oddscalculator Koomentare aktual.

--- Sports-Almanach/Utils/OddsCalculator.swift
+++ Sports-Almanach/Utils/OddsCalculator.swift
@@ -16,22 +16,21 @@ struct OddsCalculator {
         let totalGoals = homeScore + awayScore
         
         // Heimsieg
-        let homeWinOdds = totalGoals > 0 ?
-        max(1.2, Double(awayScore + 1) / Double(homeScore + 1)) :
-        2.0
+        let homeWinOdds = totalGoals > 0 ? // Ob mindestens ein Tor gefallen ist
+        max(1.2, Double(awayScore + 1) / Double(homeScore + 1)) : // Max das die Quote min 1.2
+        2.0 // Standardquote für Heimsieg, wenn keine Tore
         
         // Auswärtssieg
-        let awayWinOdds = totalGoals > 0 ?
+        let awayWinOdds = totalGoals > 0 ? // Ob mindestens ein Tor gefallen ist
         max(1.2, Double(homeScore + 1) / Double(awayScore + 1)) :
-        2.5
+        2.5 // Standardquote für Auswärtssieg, wenn keine Tore
         
         // Unentschieden
-        let drawOdds = abs(homeScore - awayScore) <= 1 ?
-        3.0 :  // Geringer Torunterschied -> niedrigere Quote für Unentschieden
+        let drawOdds = abs(homeScore - awayScore) <= 1 ? // Überprüfen des Torunterschieds
+        3.0 :  // Geringer Torunterschied (maximal 1 Tor) -> niedrigere Quote für Unentschieden
         5.0    // Größerer Torunterschied -> höhere Quote für Unentschieden
         
-        
-        // Ein Tupel mit den Wettquoten.
+        // Ein Tupel zurückgeben
         return (homeWinOdds: homeWinOdds, drawOdds: drawOdds, awayWinOdds: awayWinOdds)
     }
 }
