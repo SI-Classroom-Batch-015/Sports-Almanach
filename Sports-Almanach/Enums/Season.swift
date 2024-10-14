60b4f8970eb64c07cf3fd78af20de97f2e56097f
Michael Fleps <f.michi84.989@gmail.com>
2024-10-14 15:23:12 +0200

    Season um weitere Jahreszahlen erweitert

--- Sports-Almanach/Enums/Season.swift
+++ Sports-Almanach/Enums/Season.swift
@@ -10,8 +10,11 @@ enum Season: String, Identifiable, CaseIterable {
     case season201872018 = "2017-2018"
     case season20182019 = "2018-2019"
     case season20192020 = "2019-2020"
+    case season20202021 = "2020-2021"
+    case season20212022 = "2021-2022"
+    case season20222023 = "2022-2023"
+    case season20232024 = "2023-2024"
 
-    
     var id: String { rawValue }
     
     var year: String {
