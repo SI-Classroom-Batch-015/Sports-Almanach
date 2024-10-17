c8df3b04667810516c840535b2013e6da796c3b5
Michael Fleps <f.michi84.989@gmail.com>
2024-10-17 15:09:25 +0200

    Fehlermanagement implementiert, im Hintergrund fetchEvents aufgerufen um API Call zu triggern

--- Sports-Almanach/ViewModels/EventViewModel.swift
+++ Sports-Almanach/ViewModels/EventViewModel.swift
@@ -10,8 +10,31 @@ import Foundation
 class EventViewModel: ObservableObject {
     
     @Published var events: [Event] = []
-
+    @Published var errorMessage: String? 
+    @Published var isLoading: Bool = false
+    private let eventRepository = EventRepository()
+    
     init() {
-          self.events = MockEvents.events 
-      }
+        Task {
+            await fetchEvents(for: .current) // Standardmäßig die aktuelle Saison abrufen
+        }
+    }
+    
+    // Funktion zum Abrufen der Events
+    func fetchEvents(for season: Season) async {
+        isLoading = true
+        do {
+            let fetchedEvents = try await eventRepository.fetchEvents(for: season)
+            DispatchQueue.main.async {
+                self.events = fetchedEvents // Events in die Liste speichern
+                self.isLoading = false
+            }
+        } catch {
+            DispatchQueue.main.async {
+                self.errorMessage = "Fehler beim Abrufen der Events: \(error.localizedDescription)"
+                self.isLoading = false
+            }
+            print("Fehler: \(error)")
+        }
+    }
 }
