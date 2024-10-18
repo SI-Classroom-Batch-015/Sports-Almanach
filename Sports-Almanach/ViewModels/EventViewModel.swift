1a7065caee8bb856385172f91926abe34f2c8bd7
Michael Fleps <f.michi84.989@gmail.com>
2024-10-18 08:44:45 +0200

    loadEvents implem. damit Task nicht im init Block ausgeführt wird. Formatierte Datum-und Zeit sowie Formatierung in einen Optional gesetzt

--- Sports-Almanach/ViewModels/EventViewModel.swift
+++ Sports-Almanach/ViewModels/EventViewModel.swift
@@ -10,13 +10,17 @@ import Foundation
 class EventViewModel: ObservableObject {
     
     @Published var events: [Event] = []
-    @Published var errorMessage: String? 
+    @Published var errorMessage: String?
     @Published var isLoading: Bool = false
     private let eventRepository = EventRepository()
     
     init() {
+        loadEvents(for: .current)
+    }
+    
+    func loadEvents(for season: Season) {
         Task {
-            await fetchEvents(for: .current) // Standardmäßig die aktuelle Saison abrufen
+            await fetchEvents(for: season)
         }
     }
     
@@ -43,4 +47,35 @@ class EventViewModel: ObservableObject {
             print("Fehler: \(error)")
         }
     }
-}
+    
+    // Formatierte Datumsanzeige
+     func formattedDate(for event: Event) -> String {
+         let dateFormatter = DateFormatter()
+         dateFormatter.dateFormat = "yyyy-MM-dd" // Format der API
+         if let dateObj = dateFormatter.date(from: event.date) {
+             dateFormatter.dateStyle = .medium // Ausgabeformat
+             return dateFormatter.string(from: dateObj)
+         }
+         return event.date
+     }
+     
+     // Formatierte Zeitanzeige
+     func formattedTime(for event: Event) -> String {
+         let timeFormatter = DateFormatter()
+         timeFormatter.dateFormat = "HH:mm:ss" // Format der API
+         if let timeObj = timeFormatter.date(from: event.time) {
+             timeFormatter.timeStyle = .short // Ausgabeformat "19:00"
+             return timeFormatter.string(from: timeObj)
+         }
+         return event.time
+     }
+     
+     // Konvertiert das Score-String zu einem optionalen Int
+     func homeScoreInt(for event: Event) -> Int? {
+         return Int(event.homeScore ?? "")
+     }
+     
+     func awayScoreInt(for event: Event) -> Int? {
+         return Int(event.awayScore ?? "")
+     }
+ }
