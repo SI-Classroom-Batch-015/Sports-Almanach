5bbdeab50ae59e313763a2cee25a11941db9c615
Michael Fleps <f.michi84.989@gmail.com>
2024-10-23 07:08:58 +0200

    Beobachtbare Statevar für die Auswahl der Siegesmannschaft, bzw Unentschieden

--- Sports-Almanach/Views/List/Rows/OddsRow.swift
+++ Sports-Almanach/Views/List/Rows/OddsRow.swift
@@ -25,24 +25,39 @@ struct OddsRow: View {
                 Text("Heimsieg:")
                 Spacer()
                 Text("\(String(format: "%.2f", odds.homeWinOdds))")
+                Image(systemName: isHomeWinSelected ? "checkmark.square" : "square")
+                    .foregroundColor(.orange)
+                                   .onTapGesture {
+                                       isHomeWinSelected.toggle()
+                                   }
             }
             
             HStack {
                 Text("Unentschieden:")
                 Spacer()
                 Text("\(String(format: "%.2f", odds.drawOdds))")
+                Image(systemName: isDrawSelected ? "checkmark.square" : "square")
+                                   .foregroundColor(.orange)
+                                   .onTapGesture {
+                                       isDrawSelected.toggle()
+                                   }
             }
             
             HStack {
                 Text("Auswärtssieg:")
                 Spacer()
                 Text("\(String(format: "%.2f", odds.awayWinOdds))")
+                Image(systemName: isAwayWinSelected ? "checkmark.square" : "square")
+                                   .foregroundColor(.orange)
+                                   .onTapGesture {
+                                       isAwayWinSelected.toggle()
+                                   }
             }
         }
         .padding()
         .background(Color.gray.opacity(0.1))
         .cornerRadius(10)
-        .frame(maxWidth: .infinity, alignment: .center) // Vertical Mittig
+        .frame(maxWidth: .infinity, alignment: .center)
         
         Spacer()
     }
