5dd28d1729d2475db2df332c192bcf3a4e2986dc
Michael Fleps <f.michi84.989@gmail.com>
2024-10-24 23:59:20 +0200

    Liste zum anzeigen der events auf die gewtettet werden möchte gesetzt samt Swipe funktion

--- Sports-Almanach/Views/List/BetView.swift
+++ Sports-Almanach/Views/List/BetView.swift
@@ -15,18 +15,34 @@ struct BetView: View {
     
     var body: some View {
         
-        ZStack {
-            Image("hintergrund")
-                .resizable()
-                .scaledToFill()
-                .edgesIgnoringSafeArea(.all)
-            
-            Text("Wettoptionen")
-                .font(.largeTitle)
-                .foregroundColor(.white)
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(Color.clear)
-                .contentShape(Rectangle())
+        NavigationStack {
+            ZStack {
+                Image("hintergrund")
+                    .resizable()
+                    .scaledToFill()
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack {
+                    Text("Wettoptionen")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding(.bottom, 20)
+                    
+                    List {
+                        ForEach(eventViewModel.selectedBetEvents) { event in
+                            BetRow(eventViewModel: eventViewModel, event: event)
+                                .swipeActions(edge: .trailing, allowsFullSwipe: false) { // Swipe-Aktionen 
+                                    Button(role: .destructive) {
+                                        eventViewModel.removeFromBet(event) // Event  entfernen
+                                    } label: {
+                                        Label("Löschen", systemImage: "trash")
+                                    }
+                                }
+                        }
+                    }
+                    .listStyle(.plain)
+                }
+            }
         }
     }
 }
