2e1c3f13ae7b0aa9c8dcd460fda614e5a339c4ef
Michael Fleps <f.michi84.989@gmail.com>
2024-11-05 14:35:44 +0100

    CustomTabbar aus der ContentvView entfernt

--- Sports-Almanach/Views/ContentView.swift
+++ Sports-Almanach/Views/ContentView.swift
@@ -9,25 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     
-    // Initial wird die HomeView angezeigt
-    @State private var displayedTab: CustomTabBar.Tab = .home
+    @State private var selectedTab: Tab = .home // Start-Tab
     @EnvironmentObject var userViewModel: UserViewModel
     @EnvironmentObject var eventViewModel: EventViewModel
     
+    enum Tab {
+        case home, events, bet, statistics
+    }
+    
     var body: some View {
-        ZStack {
-            Image("hintergrund")
-                .resizable()
-                .scaledToFill()
-                .edgesIgnoringSafeArea(.all)
-            
-            NavigationStack {
-                CustomTabBar(
-                    displayedTab: $displayedTab,
-                    previousTab: displayedTab
-                )
-                .environmentObject(userViewModel)
-                .environmentObject(eventViewModel)
+        NavigationStack {
+            ZStack {
+                Image("hintergrund")
+                    .resizable()
+                    .scaledToFill()
+                    .edgesIgnoringSafeArea(.all)
+                
+                TabView(selection: $selectedTab) {
+                    
+                    HomeView()
+                        .environmentObject(userViewModel)
+                        .environmentObject(eventViewModel)
+                        .tabItem {
+                            Label("Home", systemImage: "house")
+                        }
+                        .tag(Tab.home)
+                    
+                    EventsView()
+                        .environmentObject(eventViewModel)
+                        .tabItem {
+                            Label("Events", systemImage: "calendar")
+                        }
+                        .tag(Tab.events)
+                    
+                    BetView()
+                        .environmentObject(userViewModel)
+                        .environmentObject(eventViewModel)
+                        .tabItem {
+                            Label("Wetten", systemImage: "dollarsign.circle")
+                        }
+                        .tag(Tab.bet)
+                    
+                    StatisticsView()
+                        .tabItem {
+                            Label("Statistiken", systemImage: "rectangle.and.pencil.and.ellipsis")
+                        }
+                        .tag(Tab.statistics)
+                }
+                .accentColor(.orange)
             }
         }
     }
