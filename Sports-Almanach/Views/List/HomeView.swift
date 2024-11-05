c64a556f4db1aa156aefddd725d2d6e67b3f0101
Michael Fleps <f.michi84.989@gmail.com>
2024-11-05 07:59:59 +0100

    Selectionview  mit App Intro und Einleitung erstellt, Bilder aus dem Asset, Horizontal Scrollbar gemacht

--- Sports-Almanach/Views/List/HomeView.swift
+++ Sports-Almanach/Views/List/HomeView.swift
@@ -12,6 +12,7 @@ struct HomeView: View {
     @EnvironmentObject var userViewModel: UserViewModel
     @EnvironmentObject var eventViewModel: EventViewModel
     @State private var navigateToLogin: Bool = false
+    @State private var expandedSection: String?
     
     struct BannerImage: Identifiable {
         let id = UUID()
@@ -21,17 +22,13 @@ struct HomeView: View {
     // Array der Banner-Bildern
     let bannerImages = [
         BannerImage(imageName: "bannersports"),
-        BannerImage(imageName: "bannersports2"),
-        BannerImage(imageName: "basketball"),
         BannerImage(imageName: "boxen"),
         BannerImage(imageName: "football"),
-        BannerImage(imageName: "fussball"),
         BannerImage(imageName: "golf"),
         BannerImage(imageName: "laufen"),
         BannerImage(imageName: "radfahren"),
         BannerImage(imageName: "schwimmen"),
-        BannerImage(imageName: "tennis"),
-        BannerImage(imageName: "volleyball")
+        BannerImage(imageName: "tennis")
     ]
     
     var body: some View {
@@ -49,10 +46,11 @@ struct HomeView: View {
                         userViewModel.logout()  // Logout
                     }) {
                         Image(systemName: "rectangle.portrait.and.arrow.right")
-                        .foregroundColor(.blue)                    }
+                        .foregroundColor(.blue)
+                        .padding(.top, 32)}
                     .padding(.trailing, 32)
                 }
-                .padding(.top, 48)
+                .padding(.top, 24)
                 Spacer()
             }
             VStack {
@@ -60,14 +58,33 @@ struct HomeView: View {
                     .resizable()
                     .scaledToFit()
                     .padding(.horizontal, 32)
-                    .padding(.bottom, 160)
-                Spacer()
+                    .padding(.top, 16)
+                    .padding(.bottom, 8)
                 
-                BannerScrollView(bannerImages: bannerImages)
-                                 .frame(height: 100)
+                VStack {
+                    Rectangle()
+                        .frame(height: 1)
+                        .foregroundColor(.orange)
+                        .padding(.bottom, 24)
+                    
+                    // Sektionen
+                    SectionView(sectionName: "Events", expandedSection: $expandedSection)
+                    SectionView(sectionName: "Details", expandedSection: $expandedSection)
+                    SectionView(sectionName: "Wetten", expandedSection: $expandedSection)
+                    SectionView(sectionName: "Statistiken", expandedSection: $expandedSection)
+                        .padding(.bottom, 16)
+                    Rectangle()
+                        .frame(height: 1)
+                        .foregroundColor(.orange)
+                        .padding(.bottom, 24)
+                    Spacer()
+                    BannerScrollView(bannerImages: bannerImages)
+                        .padding(.bottom, 160)
+                }
+                .frame(maxHeight: 240)
                 
-                Spacer()
             }
+            .padding(.bottom, 100)
         }
         .navigationBarBackButtonHidden(true)
         .onChange(of: userViewModel.isLoggedIn) { isLoggedIn in
@@ -81,7 +98,58 @@ struct HomeView: View {
     }
 }
 
-// Separater für ScrollView
+// Section zum ausklappen
+struct SectionView: View {
+    let sectionName: String
+    @Binding var expandedSection: String?
+    
+    var body: some View {
+        DisclosureGroup(isExpanded: Binding(
+            get: { expandedSection == sectionName },
+            set: { expandedSection = $0 ? sectionName : nil }
+        )) {
+            ScrollView {
+                switch sectionName {
+                case "Events":
+                    Text("Tauche ein in die Welt des Sports! Hier findest du eine riesige Auswahl an Events aus allen erdenklichen Sportarten. Egal ob Fußball, Basketball, Tennis oder Motorsport – hier ist für jeden etwas dabei! Wähle deine Lieblingssportart, die Liga und die Saison aus und entdecke spannende Spiele, auf die du wetten kannst. Erlebe den Nervenkitzel hautnah und fiebere mit deinen Favoriten mit!"
+                    )
+                    .font(.title3)
+                    .multilineTextAlignment(.center)
+                case "Details":
+                    Text("Hier erfährst du alles, was du wissen musst! Klicke auf ein Event und erhalte detaillierte Informationen zu den Teams, Spielern, Statistiken und vielem mehr. So kannst du fundierte Entscheidungen treffen und deine Gewinnchancen erhöhen. Wissen ist Macht – nutze die Detailansicht, um zum Experten zu werden!"
+                    )
+                    .font(.title3)
+                    .multilineTextAlignment(.center)
+                case "Wetten":
+                    Text("Zeig dein Können als Sport-Analyst! Vergleiche die Quoten, analysiere die Statistiken und platziere deine Wetten. Trau dich, auf den Außenseiter zu setzen, oder sichere dir einen kleinen Gewinn mit einer soliden Wette auf den Favoriten. Hier kannst du dein Sportwissen unter Beweis stellen und gleichzeitig tolle Gewinne abräumen!"
+                    )
+                    .font(.title3)
+                    .multilineTextAlignment(.center)
+                case "Statistiken":
+                    Text("Behalte den Überblick! In der Statistik-Ansicht siehst du, wie sich deine Wetten entwickeln. Verfolge deine Erfolge, analysiere deine Wettscheine und lerne aus deinen Fehlern. Hier findest du alle wichtigen Informationen, um deine Wettstrategie zu optimieren und deine Gewinnchancen zu maximieren. Werde zum Meister der Sportwetten!"
+                    )
+                    .font(.title3)
+                    .multilineTextAlignment(.center)
+                default:
+                    Text("Inhalt für \(sectionName)")
+                }
+            }
+            .frame(height: 100)
+        } label: {
+            Text(sectionName)
+                .font(.headline)
+                .foregroundColor(.orange)
+                .padding()
+        }
+        .background(.white.opacity(0.4))
+        .cornerRadius(8)
+        .padding(.horizontal, 32)
+        .padding(.vertical, 4)
+        .animation(.easeInOut, value: expandedSection)
+    }
+}
+
+// Horizontale ScrollView für Bilder
 struct BannerScrollView: View {
     let bannerImages: [HomeView.BannerImage]
     
@@ -92,21 +160,20 @@ struct BannerScrollView: View {
                     StyledImageView(imageName: image.imageName)
                 }
             }
-            .padding()
+            
         }
     }
 }
 
-// Separater für BannerScrollView
+// BannerBild-Ansicht
 struct StyledImageView: View {
     let imageName: String
     
     var body: some View {
         Image(imageName)
             .resizable()
-            .scaledToFit()
+            .scaledToFill()
             .frame(width: 100, height: 80)
-            .padding(4)
             .background(
                 RoundedRectangle(cornerRadius: 10)
                     .stroke(Color.orange, lineWidth: 1)
