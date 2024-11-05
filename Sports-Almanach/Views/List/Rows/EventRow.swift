0e35935ce74ad2601f1451b184853a1c09535011
Michael Fleps <f.michi84.989@gmail.com>
2024-11-05 14:58:39 +0100

    Stroke von 2 auf 1 wieder gesetzt, hatte ich schon mal

--- Sports-Almanach/Views/List/Rows/EventRow.swift
+++ Sports-Almanach/Views/List/Rows/EventRow.swift
@@ -14,50 +14,50 @@ struct EventRow: View {
     
     var body: some View {
         
-        VStack(alignment: .leading) {
-            
-            AsyncImage(url: URL(string: event.image)) { image in
-                image
-                    .resizable()
-                    .frame(maxWidth: .infinity, maxHeight: 162)
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-            } placeholder: {
-                ProgressView()
-                    .frame(height: 32)
-                    .progressViewStyle(CircularProgressViewStyle(tint: .orange))
-            }
-            
-            Spacer().frame(height: 8)
-            
-            HStack {
-                VStack(alignment: .leading) {
-                    Text(eventNameShort(event.name, limit: 34))
-                        .font(.headline)
-                        .lineLimit(1)
-                        .foregroundColor(.white)
-                    
-                    HStack(spacing: 8) {
-                        Text("\(eventViewModel.formattedDate(for: event)) um \(eventViewModel.formattedTime(for: event))")
-                            .font(.subheadline)
-                            .foregroundColor(.orange)
-                    }
+            VStack(alignment: .leading) {
+                
+                AsyncImage(url: URL(string: event.image)) { image in
+                    image
+                        .resizable()
+                        .frame(maxWidth: .infinity, maxHeight: 162)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                } placeholder: {
+                    ProgressView()
+                        .frame(height: 32)
+                        .progressViewStyle(CircularProgressViewStyle(tint: .orange))
                 }
-                Spacer() // Status nach rechts
                 
-                let status = EventStatus(rawValue: event.statusString) ?? .unknown // Fallback in Enum
+                Spacer().frame(height: 8)
                 
-                Text(status.currentStatusGerman)
-                    .font(.subheadline)
-                    .padding(6)
-                    .background(status.color)
-                    .cornerRadius(8)
-                    .foregroundColor(.white)
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text(eventNameShort(event.name, limit: 34))
+                            .font(.headline)
+                            .lineLimit(1)
+                            .foregroundColor(.white)
+                        
+                        HStack(spacing: 8) {
+                            Text("\(eventViewModel.formattedDate(for: event)) um \(eventViewModel.formattedTime(for: event))")
+                                .font(.subheadline)
+                                .foregroundColor(.orange)
+                        }
+                    }
+                    Spacer() // Status nach rechts
+                    
+                    let status = EventStatus(rawValue: event.statusString) ?? .unknown // Fallback in Enum
+                    
+                    Text(status.currentStatusGerman)
+                        .font(.subheadline)
+                        .padding(6)
+                        .background(status.color)
+                        .cornerRadius(8)
+                        .foregroundColor(.white)
+                }
             }
-        }
         .padding() // Innenabstand um gesamte Zelle
         .overlay(
             RoundedRectangle(cornerRadius: 10)
-                .stroke(Color.orange, lineWidth: 2)
+                .stroke(Color.orange, lineWidth: 1)
         )
         .padding(.vertical, 8)
     }
