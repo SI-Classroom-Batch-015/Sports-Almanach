0dce63fb36a01a75c01d34ca5c343c1d793c4d92
neo849 <f.michi84.989@gmail.com>
2025-02-08 16:39:20 +0100

    Clean Code

--- Sports-Almanach/Views/List/StatisticsView.swift
+++ Sports-Almanach/Views/List/StatisticsView.swift
@@ -22,73 +22,7 @@ struct StatisticsView: View {
                     .resizable()
                     .scaledToFill()
                     .edgesIgnoringSafeArea(.all)
-                
-                //                VStack(spacing: 16) {
-                //                    if $betViewModel.$bets == nil {
-                //                        Text("Keine Wettscheine vorhanden")
-                //                            .font(.title2)
-                //                            .foregroundColor(.white)
-                //                    } else {
-                //                        ScrollView {
-                //                            LazyVStack(spacing: 12) {
-                //                                ForEach($betViewModel.allBets) { bet in
-                //                                    BetHistoryRow(bet: bet)
-                //                                        .onTapGesture {
-                //                                            selectedBet = bet
-                //                                            showBetDetails = true
-                //                                        }
-                //                                }
-                //                            }
-                //                            .padding(.horizontal)
-                //                        }
-                //                    }
-                //                }
-                //            }
-                    .navigationTitle("Wettscheine")
-                //            .sheet(isPresented: $showBetDetails) {
-                //                if let bet = selectedBet {
-                //                    BetDetailsView(bet: bet)
-                //                        .environmentObject(betViewModel)
-                //                        .environmentObject(userViewModel)
-                //                }
-                //            }
             }
         }
     }
 }
-    
-//    // MARK: - BetHistoryRow
-//    struct BetHistoryRow: View {
-//        let bet: Bet
-//        
-//        var body: some View {
-//            HStack {
-//                VStack(alignment: .leading, spacing: 4) {
-//                    Text("#\(bet.betSlipNumber)")
-//                        .font(.headline)
-//                    Text(bet.event.name)
-//                        .font(.subheadline)
-//                }
-//                .foregroundColor(.white)
-//                
-//                Spacer()
-//                
-//                VStack(alignment: .trailing, spacing: 4) {
-//                    Text("\(bet.betAmount, specifier: "%.2f") €")
-//                        .font(.headline)
-//                    Text("Quote: \(bet.odds, specifier: "%.2f")")
-//                        .font(.subheadline)
-//                }
-//                .foregroundColor(.white)
-//            }
-//            .padding()
-//            .background(Color.blue.opacity(0.3))
-//            .cornerRadius(10)
-//            .overlay(
-//                RoundedRectangle(cornerRadius: 10)
-//                    .stroke(Color.orange, lineWidth: 1)
-//            )
-//        }
-//    }
-//}
-//
