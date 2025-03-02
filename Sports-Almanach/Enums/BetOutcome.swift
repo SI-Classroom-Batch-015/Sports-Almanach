545d2c77c9535fdfefc5f89b89b4575c113bb196
neo849 <f.michi84.989@gmail.com>
2025-03-02 01:13:46 +0100

    BetOutcome Int werte gesetzt zum späteren vergleichen der Wettergebnisse

--- Sports-Almanach/Enums/BetOutcome.swift
+++ Sports-Almanach/Enums/BetOutcome.swift
@@ -9,32 +9,24 @@ import Foundation
 import SwiftUI
 
 /// Enum für die möglichen Ergebnisse einer Wette
-enum BetOutcome: String, Identifiable, Decodable {
-    case homeWin
-    case draw
-    case awayWin
-
-    var id: String { rawValue }
+enum BetOutcome: Int, Codable {
+    case draw = 0
+    case homeWin = 1
+    case awayWin = 2
 
     var titleGerman: String {
         switch self {
-        case .homeWin:
-            return "Heimsieg"
-        case .draw:
-            return "Remi"
-        case .awayWin:
-            return "Auswärtssieg"
+        case .homeWin: return "Heimsieg"
+        case .draw: return "Remi"
+        case .awayWin: return "Auswärtssieg"
         }
     }
 
     var color: Color {
         switch self {
-        case .homeWin:
-            return .green
-        case .draw:
-            return .yellow
-        case .awayWin:
-            return .red
+        case .homeWin: return .green
+        case .draw: return .yellow
+        case .awayWin: return .blue
         }
     }
 }
