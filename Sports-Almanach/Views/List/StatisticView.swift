e1e83cadfadbd674185d24360ef9ac1056eb9609
neo849 <f.michi84.989@gmail.com>
2025-03-30 17:20:53 +0200

    #Preview und Standartwerte für die Arrays gesetzt

--- Sports-Almanach/Views/List/StatisticView.swift
+++ Sports-Almanach/Views/List/StatisticView.swift
@@ -7,52 +7,38 @@
 
 import SwiftUI
 
-// MARK: - StatisticsView
-// Hauptview für die Statistik-Funktionalität
-// Implementiert MVVM-Pattern mit ViewModels für Geschäftslogik
 struct StatisticView: View {
     
-    // MARK: - Dependencies
-    // ViewModels werden via Environment injection bereitgestellt
     @EnvironmentObject var betViewModel: BetViewModel
     @EnvironmentObject var userViewModel: UserViewModel
     
-    // MARK: - State Properties
-    // Lokale States für UI-Interaktionen
-    @State private var selectedBet: Bet?
-    @State private var showBetDetails = false
+    var betSlips: [BetSlip] = []
+    var profiles: [Profile] = []
+    @State private var showBetDetails: BetSlip?
     
-    // MARK: - Properties
-    // Environment Objects für MVVM Pattern - ermöglichen reaktive Updates
-    // State Management für lokale UI-Zustände
-
     var body: some View {
         NavigationStack {
             ZStack {
-                // Performance-Optimierung: Hintergrundbild
                 Image("hintergrund")
                     .resizable()
                     .scaledToFill()
                     .edgesIgnoringSafeArea(.all)
                 
-                // Hauptinhalt
-                Text("Comin Soon, Rangliste und \n Wettscheine anzeigen")
-                    .font(.largeTitle)
+                Text("Coming Soon, Rangliste und \n     Wettscheine anzeigen")
+                    .font(.subheadline)
                     .foregroundColor(.white)
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .background(Color.clear)
-                    .contentShape(Rectangle())
             }
-            
         }
     }
+}
 
-    // MARK: - Preview
-    struct StatisticsView_Previews: PreviewProvider {
-        static var previews: some View {
-            StatisticView()
-                .environmentObject(BetViewModel())
-                .environmentObject(UserViewModel())
-        }
-    }
+// MARK: - Preview
+#Preview {
+    StatisticView(
+        betSlips: Mocks.betSlips,
+        profiles: Mocks.profiles
+    )
+    .environmentObject(BetViewModel())
+    .environmentObject(UserViewModel())
 }
